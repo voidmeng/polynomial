5a3874be59a1a4565c6b1ca16acfe7ea38a2b764
Meng Yuxuan <ym419@ic.ac.uk>
2021-04-25 13:39:38 +0800

    addition

--- polynomials/polynomials.py
+++ polynomials/polynomials.py
@@ -1,3 +1,5 @@
+from numbers import Number
+
 class Polynomial:
     def __init__(self, coefs):
 
@@ -21,3 +23,29 @@ class Polynomial:
                   for d, c in enumerate(coefs[2:], start = 2) if c] 
 
         return " + ".join(reversed(terms)) or "0"
+
+    def __eq__(self, other) :
+
+        return self.coefficients == other.coefficients
+
+    def __add__(self, other):
+
+        if isinstance(other, Polynomial):
+        common = min(self.degree(), other.degree) + 1
+
+        coefs = tuple(a +b for a, b in zip(self.coefficients, 
+                                           other.coeffcients))
+
+        coefs += self.coefficients[common:]  + other.coefficients[common:]
+
+        return Polynomial(coefs)    
+        
+        elif instance(other, Number):
+            return Polynomial((self.coefficients[0] + other,) 
+                              + self.coefficients[1:])
+
+        else:
+            return NotImplemented                      
+
+        def__radd__(self,other):
+            return self + other                      
